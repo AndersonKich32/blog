d74384e905fdf0fb5b4ed3f520b96a921f155968
Your Name <you@example.com>
2021-01-08 12:29:45 -0300

    test de imagens

--- components/home/index.js
+++ components/home/index.js
@@ -1,10 +1,18 @@
-//import "./styles.css"
+
+import React from 'react'
+import Image from 'next/image'
 
 function Home() {
     return(
         <div className='home'>
             <h1>Meu Blog pessoal sobre RH</h1>
             <p>Autor: Joice Kich</p>
+            <Image
+                src="/foto.jpg"
+                alt="My Image"
+                width={200}
+                height={200}
+            />
         </div>
     )
 }
